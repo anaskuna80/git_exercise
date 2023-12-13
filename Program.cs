fc8ec5f7d40397ab2016d064791c50ddc801da4a
Tomas Kindahl <tomas.kindahl@molk.com>
2023-12-13 11:05:49 +0100

    La till TodoItem-klass och Lista av ditto

--- Program.cs
+++ Program.cs
@@ -2,6 +2,18 @@
 {
     internal class Program
     {
+        List<TodoItem> taskList;
+        class TodoItem
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            enum Status { Ready, Active, Done }
+            public TodoItem(string title, string description)
+            {
+                Title = title;
+                Description = description;
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Worldie!");
