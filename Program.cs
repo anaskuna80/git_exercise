4eb6085072de2acbdc0267b5ebf6caf6c39a35f3
Andreas K <andreas.kuylenstierna@gmail.com>
2023-12-13 11:20:33 +0100

    test2

--- Program.cs
+++ Program.cs
@@ -7,9 +7,11 @@ namespace git_exercise
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Worldie!");
+            Console.WriteLine("Error TODO");
             
         for (int gitLoop = 1; gitLoop <= 10; gitLoop++)            
             Console.WriteLine("-> " + gitLoop);           
         }
+        // None of this is real code, just a test...
     }
 }
