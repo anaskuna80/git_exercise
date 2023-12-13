1954a42d675f7cf52c150eff7102cfedec5a39ed
Andreas K <andreas.kuylenstierna@gmail.com>
2023-12-13 11:04:37 +0100

    test....

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace git_exercise
+﻿using System.Threading.Channels;
+
+namespace git_exercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Worldie!");
+            
+        for (int gitLoop = 1; gitLoop <= 10; gitLoop++)            
+            Console.WriteLine("-> " + gitLoop);           
         }
     }
 }
